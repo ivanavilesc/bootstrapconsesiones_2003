5f3c556893776f55ec996cf11b270152d6e6fd53
Ivancito <Ivancito@NTBK-NP500>
2019-03-17 20:06:56 -0300

    Codigo PHP, utilizando bootstrap 3.3.7 - Inclusión codigo y boton para MODAL

--- menu.php
+++ menu.php
@@ -3,6 +3,31 @@
     <a href="index.php" class="list-group-item "> <span class="fa fa-home"></span> Ir al home </a>
     <a href="contenido.php" class="list-group-item "> <span class="fa fa-book"></span> Contenido </a>
     <a href="vacaciones.php" class="list-group-item "> <span class="fa fa-chevron-right"></span>  Formulario Vacaciones </a>
-    <a href="#" class="list-group-item "> Opción 3 </a>
-    <a href="#" class="list-group-item "> Opción 4 </a>            
+    <a href="#" class="list-group-item "> Opción 3 </a>   
+    <a href="#" class="list-group-item "> Opción 4 </a>          
+    <!--
+    
+    Boton MODAL
+    <a href="#" class="list-group-item " data-toggle="modal" data-target="#myModal" >  Opción 3 </a>
+    
+    Codigo MODAL
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    -->
+    
 </ul>
